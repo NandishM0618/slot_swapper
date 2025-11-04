99e5332964de1173f6bc922ea92b3bdda8179304
NandishM0618 <nandish1729@gmail.com>
2025-11-04 20:45:35 +0530

    feat(backend): connect database and add user routes

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
+const connectDB = require('./config/db')
 
 const app = express()
+
+const userRoutes = require('./routes/user')
+
+dotenv.config()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+connectDB()
 
 app.get('/', (req, res) => {
     res.json({ message: "Hello" })
 })
+
+app.use("/api/users", userRoutes);
 
 app.listen(8080, () => console.log("Listening on port 8080"));
